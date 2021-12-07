0707bd3f3c5f1bb3cb4af94ca89608e1f969eaff
celestin <46505055+celestin98@users.noreply.github.com>
2021-12-07 21:18:10 +0100

    finish exercice2

--- src/app/component/new-post/new-post.component.ts
+++ src/app/component/new-post/new-post.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
 import {PostService} from "../../service/post.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-new-post',
@@ -8,26 +9,27 @@ import {PostService} from "../../service/post.service";
   styleUrls: ['./new-post.component.scss']
 })
 export class NewPostComponent implements OnInit {
-  postForm=new FormGroup({
+  postForm = new FormGroup({
     title: new FormControl(''),
-    content: new  FormControl('')
+    content: new FormControl('')
   })
 
-  constructor(private postService:PostService) { }
+  constructor(private postService: PostService, private router: Router) {
+  }
 
   ngOnInit(): void {
   }
 
-  submit(){
-        this.postService.addPost(
-          {
-            title:this.postForm.getRawValue().title,
-            content: this.postForm.getRawValue().content,
-            loveIts: 0,
-            created_at: Date.now()
-          }
-        )
-          
+  submit() {
+    this.postService.addPost(
+      {
+        title: this.postForm.getRawValue().title,
+        content: this.postForm.getRawValue().content,
+        loveIts: 0,
+        created_at: Date.now()
+      }
+    )
+    this.router.navigate(['exercice1'])
   }
 
 }
